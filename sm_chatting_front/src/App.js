c07a48f7bc7a072d093c2f695ed114e1b496e5ea
denist2322 <parksomang125@gmail.com>
2022-09-03 23:52:20 +0900

    메인페이지 작업(로그인 페이지)

--- sm_chatting_front/src/App.js
+++ sm_chatting_front/src/App.js
@@ -1,25 +1,10 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import axios from "axios";
+import MainImagePage from "./components/MainImagePage.jsx";
 
 function App() {
- const [message, setMessage] = useState("");
-
- useEffect(() => {
-  const getData = async () => {
-   const BackData = await axios({
-    url: "http://localhost:8031/test",
-    method: "GET",
-   });
-   setMessage(BackData.data);
-  };
-  getData();
- }, []);
-
  return (
   <div className="App">
-   <h1>{message}</h1>
-   <div>안녕</div>
+   <MainImagePage />
   </div>
  );
 }
