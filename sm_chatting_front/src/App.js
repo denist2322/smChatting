0c1b7883dc7234977964e22e981758653a939d57
denist2322 <parksomang125@gmail.com>
2022-09-07 09:21:32 +0900

    라우터 연결

--- sm_chatting_front/src/App.js
+++ sm_chatting_front/src/App.js
@@ -2,13 +2,18 @@ import "./App.css";
 import MainPage from "./components/MainPage.jsx";
 import MessengerPage from "./components/Messenger.jsx";
 import JoinPage from "./components/JoinPage.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
- return (
-  <div className="App">
-   <JoinPage />
-  </div>
- );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/MessengerPage" element={<MessengerPage />}></Route>
+        <Route path="/JoinPage" element={<JoinPage />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
