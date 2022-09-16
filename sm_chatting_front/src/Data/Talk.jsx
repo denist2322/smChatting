3652f1d3f700c3575b4816992cfd938e03336d83
denist2322 <parksomang125@gmail.com>
2022-09-16 13:12:34 +0900

    버그 수정 완료

--- sm_chatting_front/src/Data/Talk.jsx
+++ sm_chatting_front/src/Data/Talk.jsx
@@ -1,55 +1,66 @@
-import SockJS from 'sockjs-client'
-import Stomp from 'stompjs';
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
 import React, { useState, useEffect } from "react";
 
 const Talk = () => {
-  const [msg,setMsg] = useState([]);
-  const [content,setContent] = useState("");
-  let socket = new SockJS('http:/localhost:8031/chat/chatting');
+  const [msg, setMsg] = useState([]);
+  const [content, setContent] = useState("");
+  let socket = new SockJS("http:/localhost:8031/chat/chatting");
   let client = Stomp.over(socket);
 
-  useEffect(() => {    
-    client.connect({}, ()=>{   
+  useEffect(() => {
+    client.connect({}, () => {
       // 연결시 jwt를 보냄
       // client.send("/app/join", {} ,JSON.stringify(localStorage.getItem("Token")))
-      client.subscribe("/queue/addChatToClient" , function(Message){
-        const newMsg = JSON.parse(Message.body).content;
-        console.log("지금 상태는 : " , msg)
-        const newMsgs = [...msg];
-        newMsgs.push(newMsg);
-        setMsg(newMsgs);
-      })
-    })
-  }, [msg]);
 
- 
+      // (초기 셋팅)처음 들어오면 DB에 있는 메시지를 추출함
+      client.send(`/app/first`, {});
+
+      //
+      client.subscribe("/queue/firstChat", function (Message) {
+        const newMsg = JSON.parse(Message.body).map((a) => a.content);
+        setMsg(newMsg);
+      });
+
+      client.subscribe("/queue/addChatToClient", function (Message) {
+        const newMsg = JSON.parse(Message.body).content;
+        setMsg((prev) => prev.concat(newMsg));
+      });
+    });
+  }, []);
 
   const handleSubmit = (e, content) => {
     e.preventDefault();
-    console.log("들어옴");
-    client.send(`/app/chat` , {}, JSON.stringify({content}))
-  }
+    client.send(`/app/chat`, {}, JSON.stringify({ content }));
+    setContent("");
+  };
 
-  let msgList = msg.map((_msg, index) => (<li key={index}>{_msg}</li>))
-
-  return(
+  return (
     <div>
       <div>채팅</div>
       <form onSubmit={(e) => handleSubmit(e, content)}>
         <div>
-          <input 
-              type="text"
-              placeholder='내용을 입력하세요.' 
-              onChange={(e) => {
-                setContent(e.target.value)
-            }}/>
+          <input
+            type="text"
+            placeholder="내용을 입력하세요."
+            value={content}
+            onChange={(e) => {
+              setContent(e.target.value);
+            }}
+          />
         </div>
         <button type="submit">제출</button>
         <div>내용</div>
-        <div>{msgList}</div>
+        <div>
+          <ul>
+            {msg.map((_msg, index) => (
+              <li key={index}>{_msg}</li>
+            ))}
+          </ul>
+        </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default Talk;
